21c8f911aae49f5d1c0dff140e3a58abce8e9ea2
Zeitlin1 <Zeitlin1@gmail.com>
2016-09-21 12:19:43 -0400

    Add files via upload
    Test files for Unicorn emoji

---
+++ ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Unicorn
+//
+//  Created by James Campagno on 7/28/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // We provided the code for you!
+    
+    func unicorn() -> String {
+        
+        return "🦄 🦄 🦄 🦄"
+        
+    }
+    
+    func loveAgain(_ person: String) -> String {
+        
+        return person + "♥️"
+        
+    }
+    
+}
